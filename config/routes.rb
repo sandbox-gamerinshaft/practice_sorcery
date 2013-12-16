b836db27af92c18a8fdfcb250290546c05e6cf10
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-16 17:40:43 +0900

    rails g sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sorcery::Application.routes.draw do
+  get "sessions/new"
   resource :registrations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
